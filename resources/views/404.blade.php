ad4974905130ab54d6eb08bdee891181a6af627c
Chaimae <chaimaechabouti@gamil.com>
2026-01-08 17:09:29 +0100

    404

---
+++ resources/views/404.blade.php
@@ -0,0 +1,11 @@
+@extends('Master_page')
+
+@section('title', '404')
+
+@section('content')
+<div class="text-center">
+    <h1 class="display-4 text-danger">404</h1>
+    <p class="lead">Page introuvable</p>
+    <a href="/" class="btn btn-primary">Retour à l'accueil</a>
+</div>
+@endsection
